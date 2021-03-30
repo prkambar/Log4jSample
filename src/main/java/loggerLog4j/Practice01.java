ee7fc839a71b6f997c370e163b922a8a97552c28
prkambar <prkambar@Tejasit-PC8.TISPL.COM>
2021-03-30 16:48:55 +0530

    updated the Practice01.class

--- src/main/java/loggerLog4j/Practice01.java
+++ src/main/java/loggerLog4j/Practice01.java
@@ -1,10 +1,34 @@
 package loggerLog4j;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.HTMLLayout;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+
 public class Practice01 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	//Create global logger 
+	private static Logger log = Logger.getLogger(Practice01.class);
+	
+	public static void main(String[] args) throws Exception {
+		// First Create Layout 
+		Layout layout = new HTMLLayout();
+		
+		// Second Create Appender and Link 
+		Appender append = new FileAppender(layout, "D:/mylogs/processLogs.html");
+		
+		// Fourth Add Appender 
+		log.addAppender(append);
+	
+		log.info("Application Started.");
+		log.info("Login successful by user.");
+		log.info("Ticket is booked");
+		log.info("Payment Recevied sucessfully.");
+		log.info("Ticket sent over email and SMS");
+		log.info("Ticket Booking process is completed");
+		log.info("Application Logout.");
+		
 	}
 
 }
