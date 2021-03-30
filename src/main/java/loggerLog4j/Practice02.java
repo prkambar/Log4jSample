a8873e5e0df57b3529b7d786dd47bbbccdd09e8c
prkambar <prkambar@Tejasit-PC8.TISPL.COM>
2021-03-30 17:09:39 +0530

    ConsoleAppender

---
+++ src/main/java/loggerLog4j/Practice02.java
@@ -0,0 +1,28 @@
+package loggerLog4j;
+
+import java.lang.invoke.MethodHandleInfo;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+
+public class Practice02 {
+	
+	private static Logger log = Logger.getLogger(Practice02.class);
+	
+	public static void main(String[] args) {
+		
+		Layout ly = new SimpleLayout();
+		
+		Appender  append = new ConsoleAppender(ly);
+		
+		log.addAppender(append);
+		
+		log.debug(append);
+	}
+		
+}
+
+
